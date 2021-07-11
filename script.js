56fb3786ff3cb31e8a16720fb8ce65d982f2706e
Rukhsar Saba <saba.rukhsar93@gmail.com>
2021-07-11 22:33:08 +0530

    remove comment

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// const closeButton = document.querySelector('.close-nav');
-// const openButton = document.querySelector('.open-nav');
-// const nav = document.querySelector('.nav');
-
-// closeButton.addEventListener("click", () => {
-//     nav.classList.remove('navigation-open');
-// });
-
-// openButton.addEventListener("click", () => {
-//     nav.classList.add('navigation-open');
-// });
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
